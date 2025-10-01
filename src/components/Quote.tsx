c406e45c52eb9a8b26544eda067513c953286e41
Tejas Ekhande <tejasekhande01@gmail.com>
2025-10-01 12:18:59 +0530

    Initialize project and add quote component

---
+++ src/components/Quote.tsx
@@ -0,0 +1,16 @@
+function Quote() {
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center items-center">
+            <div className="max-w-xl text-center">
+                <h1 className="text-3xl font-semibold text-gray-800">
+                    “The best way to get started is to quit talking and begin doing.”
+                </h1>
+                <p className="mt-4 text-lg text-gray-600 font-medium">
+                    — Walt Disney
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Quote;
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import Quote from "../components/Quote.tsx";
+
+function Signup() {
+    return (
+        <>
+            <Quote/>
+        </>
+    )
+}
+
+export default Signup;
