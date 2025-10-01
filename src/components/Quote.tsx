b57fba18a533b80aa4f9a2fdc0b443800f248f36
Tejas Ekhande <tejasekhande01@gmail.com>
2025-10-01 12:38:30 +0530

    Make signup page responsive

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,7 +1,7 @@
 function Quote() {
     return (
         <div className="bg-slate-200 h-screen flex justify-center items-center">
-            <div className="max-w-xl text-center">
+            <div className="max-w-xl text-center p-1">
                 <h1 className="text-3xl font-semibold text-gray-800">
                     “The best way to get started is to quit talking and begin doing.”
                 </h1>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,9 +2,15 @@ import Quote from "../components/Quote.tsx";
 
 function Signup() {
     return (
-        <>
-            <Quote/>
-        </>
+        <div className="grid grid-cols-12 h-screen">
+            <div className="col-span-12 md:col-span-5">
+                <h1 className="text-2xl font-semibold p-8">Signup Form</h1>
+            </div>
+
+            <div className="col-span-7 invisible md:visible">
+                <Quote />
+            </div>
+        </div>
     )
 }
 
